c6abb918168190f06875dfeb611b10f1153d1e46
Danil Yatsenko <danil.yasenko.1997@mail.ru>
2020-03-28 04:22:59 +0300

    Урок 2 обязательное выполнен

--- js/main.js
+++ js/main.js
@@ -1,8 +1,31 @@
-let money; 
-let income;
-let addExpenses;
-let deposit;
-let mission;
-let period;
-alert("Hello world");
-console.log(1);
+// Доход за месяц
+let money = 20000; 
+// Вывод типа данных переменной money
+console.log (typeof money);
+// Дополнительный доход
+let income = 'ютуб';
+// Вывод типа данных переменной income
+console.log(typeof income);
+// Дополнительные расходы
+let addExpenses = 'еда, одежда, развлечения';
+//  Вывод длинны строки
+console.log(addExpenses.length);
+// Привод строки к нижнему регистру
+console.log(addExpenses.toLowerCase());
+// Вывод строки в виде массива
+console.log(addExpenses.split(', '));
+// Булевое значение
+let deposit = true;
+// Вывод типа данных переменной deposit
+console.log(typeof deposit);
+// Сумма которую надо накопить
+let mission = 200000;
+// Число от 1 до 12
+let period = 12;
+//Вывод  “Период равен (period) месяцев” 
+console.log('Период равен ' + period +' месяцев');
+// Объявление переменной budgetDay
+let budgetDay;
+// Дневной бюджет
+budgetDay = 1000;
+console.log(budgetDay);
