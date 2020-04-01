9e646d5847365e1a9cf884088556081937ce000d
Danil Yatsenko <danil.yasenko.1997@mail.ru>
2020-04-01 20:17:47 +0300

    урок 4 выполнен

--- js/main.js
+++ js/main.js
@@ -3,12 +3,8 @@
 // Доход за месяц
 let money = 20000; 
 // Вывод типа данных переменной money
-console.log (typeof money);
-// Дополнительный доход
 let income = 'ютуб';
 // Вывод типа данных переменной income
-console.log(typeof income);
-// Дополнительные расходы
 let addExpenses = 'еда, одежда, развлечения';
 //  Вывод длинны строки
 console.log(addExpenses.length);
@@ -18,8 +14,6 @@ console.log(addExpenses.toLowerCase());
 console.log(addExpenses.split(', '));
 // Булевое значение
 let deposit = true;
-// Вывод типа данных переменной deposit
-console.log(typeof deposit);
 // Сумма которую надо накопить
 let mission = 200000;
 // Число от 1 до 12
@@ -50,20 +44,59 @@ let amount1 = prompt('Во сколько это обойдется?');
 let expenses2 = prompt('Введите обязательную статью расходов?');
 let amount2 = prompt('Во сколько это обойдется?');
 
-// Бюджет на месяц
-let budgetMonth = money - (amount1 - amount2);
-console.log('Бюджет на месяц', budgetMonth)
 
-//Рассчёт количества  месяцев для достижения цели
-console.log('цель будет достигнута за ', Math.ceil(mission / budgetMonth),' месяца(ев)');
 
+// Начало 4 урока
+// Вывод типов данных(по уроку)
+let showTypeOf = function (data) {
+  return(data, typeof (data));
+};
+console.log(showTypeOf(money));
+console.log(showTypeOf(income));
+console.log(showTypeOf(deposit));
+
+
+// Завернул рассчёт уровня дохода в функцию(по уроку) 
+let getStatusIncome = function name() {
+  if (budgetDay >= 1200) return('У вас высокий уровень дохода');
+  if (budgetDay >= 600 && budgetDay <= 1200) return ('У вас средний уровень дохода');
+  if (budgetDay <= 600) return ('К сожалению у вас уровень дохода ниже среднего');
+  if (budgetDay <= 0) return ('Что то пошло не так');
+};
+console.log(getStatusIncome());
+
+
+
+//1) Функция возвращает сумму всех обязательных расходов за месяц
+function getExpensesMonth(amount1, amount2) {
+  return +amount1 + +amount2;
+}
+getExpensesMonth();
+console.log(getExpensesMonth(amount1, amount2));
+
+// 2)Функция возвращает Накопления за месяц (Доходы минус расходы)
+let accumulatedMonth;
+function getAccumulatedMonth(money, amount1, amount2) {
+
+// 3) Результат вызова в переменную
+ accumulatedMonth = +money - (+amount1 + +amount2);
+ return accumulatedMonth;
+}
+getAccumulatedMonth();
+console.log(getAccumulatedMonth(money, amount1, amount2));
+
+//4) Функция Подсчитывает за какой период будет достигнута цель, зная результат месячного накопления 
+
+function getTargetMonth(mission, accumulatedMonth) {
+  return (Math.ceil(mission / accumulatedMonth));
+};
+// Тут создал дополнительную переменную для работы
+let res = getTargetMonth(mission, accumulatedMonth);
+console.log('цель будет достигнута за', res, ' месяца(ев)');
+
+// 5-6) Удалял  не нужный код.   и удалил переменную  budgetDay 
 // Рассчёт дневного бюджета, учитывая бюджет на месяц 
-budgetDay =  budgetMonth / 30;
+budgetDay = accumulatedMonth / 30;
 
-// Вывод в консоль дневного бюджета с округлением
+// Вывод в консоль дневного бюджета с округлением(перенёс сверху вниз)
 console.log('Бюджет на день', Math.floor(budgetDay));
-
-if (budgetDay >= 1200) console.log('У вас высокий уровень дохода');
-if (budgetDay >= 600 && budgetDay <= 1200) console.log('У вас средний уровень дохода');
-if (budgetDay <= 600) console.log('К сожалению у вас уровень дохода ниже среднего');
-if (budgetDay <= 0) console.log('Что то пошло не так');
