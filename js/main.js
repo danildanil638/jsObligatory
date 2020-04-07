d88448a13c18497e03d2d0261e72e8291e113042
Danil Yatsenko <danil.yasenko.1997@mail.ru>
2020-04-07 21:57:54 +0300

    урок 7

--- js/main.js
+++ js/main.js
@@ -29,53 +29,50 @@ let appData = {
    let addExpenses = prompt('Перечислите возможные расходы за рассчитываемый период через запятую');
       appData.addExpenses = addExpenses.toLowerCase().split(',');
       appData.deposit = confirm('Есть ли у вас депозит в банке?');
-  },
-  getExpensesMonth:
-    function getExpensesMonth() {
     let sum;
     let sum2 = 0;
-
     for (let i = 0; i < 2; i++) {
 
-      expenses[i] = prompt('Введите обязательную статью расходов?');
+      appData.expenses[i] = prompt('Введите обязательную статью расходов?');
       do {
         sum = prompt('Во сколько это обойдётся');
       }
       while (!isNumber(sum));
       sum2 += +sum;
     }
-    console.log(expenses);
+    console.log(appData.expenses);
     return sum2;
   },
-  getAccumulatedMonth: 
-
-  function getAccumulatedMont () {
+  getExpensesMonth: function () {
+      for (let key in appData.expenses) {
+        appData.expensesMonth = (key + sum);
+      };
+    },
+  getAccumulatedMonth: function  () {
     return money - expensesAmount;
   },
-  getTargetMonth:
-
-  function getTargetMonth() {
+  getTargetMonth: function () {
     return (Math.ceil(appData.mission / accumulatedMonth));
   },
-  getStatusIncome: 
-  
-  function getStatusIncome() {
+  getStatusIncome: function () {
      if (budgetDay >= 1200) return ('У вас высокий уровень дохода');
      if (budgetDay >= 600 && budgetDay <= 1200) return ('У вас средний уровень дохода');
      if (budgetDay <= 600) return ('К сожалению у вас уровень дохода ниже среднего');
      if (budgetDay <= 0) return ('Что то пошло не так');
   }
 };
-
+  
 appData.asking();
+
+
 
 
 
 //Вывод  “Период равен (period) месяцев” 
 console.log('Период равен ' + appData.period +' месяцев');
 // Объявление переменной budgetDay
-let expenses = [];
-let expensesAmount = appData.getExpensesMonth();
+appData.expenses = [];
+appData.expensesAmount = appData.expenses();
 console.log('Расходы за месяц: ' + expensesAmount);
 let accumulatedMonth = appData.getAccumulatedMonth();
 let res = appData.getTargetMonth();
