95492b1746f2b4236278001d3ccc95eeacc2b592
Danil Yatsenko <danil.yasenko.1997@mail.ru>
2020-04-12 00:24:12 +0300

    файлик js потерялся

---
+++ main.js
@@ -0,0 +1,40 @@
+'use strict';
+
+console.log(document.getElementById('start'));
+console.log(document.getElementsByTagName('button')[0]);
+console.log(document.getElementsByTagName('button')[1]);
+console.log(document.querySelector('.deposit-checkmark'));
+console.log(document.querySelectorAll('.additional_income-item'));
+console.log(document.getElementsByClassName('budget_month-value'));
+console.log(document.getElementsByClassName('budget_day-value'));
+console.log(document.getElementsByClassName('expenses_month-value'));
+console.log(document.getElementsByClassName('additional_income-value'));
+console.log(document.getElementsByClassName('additional_expenses-value'));
+console.log(document.getElementsByClassName('income_period-value'));
+console.log(document.getElementsByClassName('target_month-value'));
+
+let salaryAmount = document.querySelector('.salary-amount');
+
+console.log(salaryAmount);
+
+let incomeTitle = document.querySelector('.income-title');
+
+console.log(incomeTitle);
+
+let incomeAmount = document.querySelector('.income-amount');
+
+console.log(incomeAmount);
+
+let additionalExpensesItem = document.querySelector('.additional_expenses-item');
+
+console.log(additionalExpensesItem);
+
+
+let targetAmount = document.querySelector('.target-amount');
+
+console.log(targetAmount);
+
+
+let periodSelect = document.querySelector('.period-select');
+
+console.log(periodSelect);
